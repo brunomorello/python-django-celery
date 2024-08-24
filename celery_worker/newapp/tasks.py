ba7f0419e1d5a902f97ec18f3aa78498fe89ae9e
Bruno Moreno <brunomorello7@gmail.com>
2024-08-24 15:44:35 +0100

    created custom script to monitor django webapp it creates an issue on Sentry using a dedicated celery worker

--- celery_worker/newapp/tasks.py
+++ celery_worker/newapp/tasks.py
@@ -1,5 +1,14 @@
+import requests
+
 from celery import shared_task
+from sentry_sdk import capture_exception
 
 @shared_task
-def task2(): 
-    return
+def check_webpage(): 
+    try:
+        response = requests.get("http://127.0.0.1:8001")
+        if response.status_code != 200:
+            raise Exception(f"Website is down!! PANIC!!")
+    except requests.exceptions.RequestException as e:
+        capture_exception(e)
+        
